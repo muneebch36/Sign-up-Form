2461b0111c268137a02767ebe8aed4a5fac58f64
yomunnib <muneeb.cheema36@gmail.com>
2023-01-07 12:20:13 -0500

    Finish banner overlay and phone media sizing in css.

---
+++ script.js
@@ -0,0 +1,4 @@
+const pass = document.getElementById("pass");
+const passConfirm = document.getElementById("pass-confirm");
+const form = document.getElementById("new-worshipper");
+form.noValidate = true;
